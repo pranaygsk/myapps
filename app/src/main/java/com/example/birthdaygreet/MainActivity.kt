0e1bfd0f9c016290c95d4f45f7944f08ebf74c87
pranay gsk <pranaygsk97@gmail.com>
2022-09-20 13:01:37 +0530

    git add activity_main.xml

--- app/src/main/java/com/example/birthdaygreet/MainActivity.kt
+++ app/src/main/java/com/example/birthdaygreet/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.birthdaygreet
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {
+        val name= nameInput
+        Toast.makeText(this,"Button was clicked...",Toast.LENGTH_LONG).show()
     }
 }
